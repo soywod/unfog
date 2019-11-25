0208de9a1a16d50cf744add6a2474fd1a6974dd6
Clément DOUIN <soywod@users.noreply.github.com>
2019-11-25 18:23:16 +0100

    add travis conf for cross-building

--- release.nix
+++ release.nix
@@ -3,16 +3,16 @@ let
   pkgs = import <nixpkgs> {};
   haskellNix = import (fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz);
 
-  # pkgsNative = import <nixpkgs> haskellNix;
+  pkgsNative = import <nixpkgs> haskellNix;
   # pkgsLinux = import <nixpkgs> (haskellNix // { crossSystem = pkgs.lib.systems.examples.musl64; });
-  pkgsWindows = import <nixpkgs> (haskellNix // { crossSystem = pkgs.lib.systems.examples.mingwW64 ;});
+  # pkgsWindows = import <nixpkgs> (haskellNix // { crossSystem = pkgs.lib.systems.examples.mingwW64 ;});
 
-  # native = unfog { pkgs = pkgsNative; };
+  native = unfog { pkgs = pkgsNative; };
   # crossLinux = unfog { pkgs = pkgsLinux; };
-  crossWindows = unfog { pkgs = pkgsWindows; };
+  # crossWindows = unfog { pkgs = pkgsWindows; };
 
 in {
-  # unfogNative = native.unfog.components.exes.unfog;
+  unfogNative = native.unfog.components.exes.unfog;
   # unfogLinux = crossLinux.unfog.components.exes.unfog;
-  unfogWindows = crossWindows.unfog.components.exes.unfog;
+  # unfogWindows = crossWindows.unfog.components.exes.unfog;
 }
