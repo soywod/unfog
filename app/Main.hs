032ac63c6efa4c05b6e5b8e889fa65e6a0487ba1
Clément DOUIN <soywod@users.noreply.github.com>
2019-11-28 14:51:27 +0100

    init POC parser with readP

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,78 @@
 module Main where
 
-import           Data.List                      ( (\\) )
+import           Prelude                 hiding ( Word )
+import           Data.List                      ( (\\)
+                                                , intercalate
+                                                )
 import           System.Environment             ( getArgs )
 
 import qualified Command                       as C
 import qualified Query                         as Q
+import           Data.Char
 import           Response
+import           Control.Applicative     hiding ( many )
+import           Text.ParserCombinators.ReadP
+
+data Arg
+  = Cmd String
+  | AddTag String
+  | DelTag String
+  | Desc String
+  deriving Show
+
+type Cmd = String
+type Word = String
+type Tag = String
+data State = State { _cmd :: Cmd, _desc :: [Word], _tags :: [Tag] } deriving Show
+emptyState = State { _cmd = "help", _desc = [], _tags = [] }
+
+isAlphaNumOrDash c | isAlphaNum c  = True
+                   | c `elem` "-_" = True
+                   | otherwise     = False
+
+cmdExpr = string "cmd1" <|> string "cmd2"
+
+addTagExpr = do
+  satisfy (== '+')
+  tag <- munch1 isAlphaNumOrDash
+  skipSpaces
+  return tag
+
+delTagExpr = do
+  satisfy (== '-')
+  tag <- munch1 isAlphaNumOrDash
+  skipSpaces
+  return tag
+
+wordExpr = do
+  fchar <- satisfy $ not . flip elem "+-"
+  word  <- munch1 (/= ' ')
+  skipSpaces
+  return $ fchar : word
+
+parser :: ReadP [Arg]
+parser = do
+  cmd <- Cmd <$> cmdExpr
+  skipSpaces
+  args <- many1 $ addTag <|> delTag <|> word
+  return $ cmd : args
+ where
+  addTag = AddTag <$> addTagExpr
+  delTag = DelTag <$> delTagExpr
+  word   = Desc <$> wordExpr
+
+eval :: State -> Arg -> State
+eval state arg = case arg of
+  Cmd    cmd  -> state { _cmd = cmd }
+  AddTag tag  -> state { _tags = _tags state ++ [tag] }
+  DelTag tag  -> state { _tags = _tags state \\ [tag] }
+  Desc   desc -> state { _desc = _desc state ++ [desc] }
+
+runParser :: ReadP [Arg] -> String -> State
+runParser p "" = error "Command missing."
+runParser p s  = case readP_to_S p s of
+  []  -> error "Command not found"
+  res -> foldl eval emptyState $ fst . last $ res
 
 main :: IO ()
 main = getArgs >>= dispatch
