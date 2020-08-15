6173e055cc6a3799b9caf88ef547d0142d07779e
Vincent Cordobes <vincent.cordobes@gmail.com>
2020-08-15 11:11:37 +0200

    fix macos install script #38

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [1.0.0]
+## [1.0.0] - 2020-08-14
 
 The args parser becomes more solid (based on
 [optparse](https://github.com/pcapriotti/optparse-applicative#custom-parsing-and-error-handling)
--- bin/install.sh
+++ bin/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 get_os () {
-  if [ "$OSTYPE" == "linux-gnu" ]; then
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "linux"
-  elif [ "$OSTYPE" == "darwin"* ]; then
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "osx"
-  elif [ "$OSTYPE" == "cygwin" ]; then
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo "windows"
-  elif [ "$OSTYPE" == "msys" ]; then
+  elif [[ "$OSTYPE" == "msys" ]]; then
     echo "windows"
-  elif [ "$OSTYPE" == "win32" ]; then
+  elif [[ "$OSTYPE" == "win32" ]]; then
     echo "windows"
-  elif [ "$OSTYPE" == "freebsd"* ]; then
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
     echo "linux"
   else
     return -1
@@ -23,11 +23,16 @@ os=`get_os`
 echo "Downloading latest ${os} release..."
 curl -sLo unfog.tar.gz "https://github.com/soywod/unfog/releases/latest/download/unfog-${os}.tar.gz"
 echo "Installing binaries..."
-tar xzf unfog.tar.gz
+tar -xzf unfog.tar.gz
 rm unfog.tar.gz
 chmod u+x unfog*
 sudo mv unfog* /usr/local/bin/
-curl -sLo unfog-completion.bash "https://raw.githubusercontent.com/soywod/unfog/master/bin/completion.bash"
-source unfog-completion.bash
-sudo mv unfog-completion.bash /etc/bash_completion.d/
-echo "Unfog installed!"
+
+if [ -d /etc/bash_completion.d/ ]; then
+  echo "Installing bash completion..."
+  unfog --bash-completion-script `which unfog` > unfog.bash
+  sudo mv unfog.bash /etc/bash_completion.d/
+  source /etc/bash_completion.d/unfog.bash
+fi
+
+echo "Unfog v$(unfog version) installed!"
