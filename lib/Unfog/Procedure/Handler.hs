d34805ca1e77c5e59e8495531f689a532f7d08cf
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-05-05 13:04:02 +0300

    Fix `worktime --more` after adding PROJECT column (#59)

--- lib/Unfog/Procedure/Handler.hs
+++ lib/Unfog/Procedure/Handler.hs
@@ -7,7 +7,7 @@ import Unfog.Response
 import qualified Unfog.State as State
 
 showVersion :: Arg.JsonOpt -> IO ()
-showVersion jsonOpt = send rtype $ VersionResponse "1.0.5"
+showVersion jsonOpt = send rtype $ VersionResponse "1.0.6"
   where
     rtype = parseResponseType jsonOpt
 
